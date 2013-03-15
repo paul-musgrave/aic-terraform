8a68b383c1b5567d66a08a41ce4918ba128dbada
paulmusgrave <musgrave.paul1@gmail.com>
2013-03-15 18:56:43 -0400

    objectified simplegame, added some mechanics

--- simplegame.py
+++ simplegame.py
@@ -1,132 +1,195 @@
 import json, subprocess, argparse
+from collections import deque
 
-# Processes a turn for bot (id), given the bot's input botInput
-def doTurn(id, botInput):
-    x, y, to = botInput.split()
-    if(to == 's'): ## blank tile, doesn't spread
-        gameMap[x][y] = {'terrain': 's', 'owner': None}
-    else:
-        propagateNano(x, y, {'terrain': to, 'owner': id if TERRAIN[to] else None})
+# Notes:
+# Tiles are somewhat annoying to handle properly; different places
+# care about different attributes, and storing properties such as nano
+# in e.g. owned tiles that won't be updated is messy.
 
 
-# given a nanoswarm at (x, y), transforms the type it is sitting on to
-# the type given by to, and propagates instantly. (NEEDS CHANGING TO TIMESTEP MODEL)
+class Terraform(object):
+    """A simple terraform engine"""
+    TERRAIN = {
+        # generic terrain types
+        1: False,
+        2: False,
+        3: False,
+        # owned terrain types
+        'f': True,
+        'c': True
+    }
 
-# naive implementation loops infinitely on x->x nano (shouldn't exist)
-def propagateNano(x, y, to):
-    prev = gameMap[x][y]
-    gameMap[x][y] = to
-    for i in xrange(-1,1):
-        for j in xrange(-1,1):
-            if(inBounds(x+i,y+j) and gameMap[x+i][x+j] == prev):
-                propagateNano(x+i, y+j, to)
+    def __init__(self, mapFile, maxTurn=1000, viewRad=5):
+        super(Terraform, self).__init__()
+        self.nanoQueue = deque()
+        self.bots = {}
+        # alternately, these could be map metadata
+        self.maxTurn = maxTurn
+        self.viewRad = viewRad
+        viewRad2 = viewRad*viewRad
+        maskSize = 2*viewRad + 1
+        viewMask = [[False] * maskSize] * maskSize
+        for i in xrange(0, viewRad):
+            for j in xrange(0, viewRad):
+                if (i*i + j*j <= viewRad2):
+                    viewMask[viewRad+i][viewRad+j] = True
+                    viewMask[viewRad+i][viewRad-j] = True
+                    viewMask[viewRad-i][viewRad+j] = True
+                    viewMask[viewRad-i][viewRad-j] = True
+        self.viewMask = viewMask
 
-    to['x'] = x, to['y'] = y
-    if(prev['terrain'] == 'H'): #check HQ destruction
-        killBot(prev['owner'])
-    if(prev['owner'] != None):
-        bots[prev['owner']]['tiles'].remove(to) #@@
-    if(to['owner'] != None):
-        bots[to['owner']]['tiles'].append(to)
-    replay[-1].append(to)
+        # currently assuming a 2d arrray; in python apparently can be a dict keyed by pairs, is this better?
+        # less convenient to load, though: can't be represented in json
+        f = open(mapFile, 'r')
+        self.gameMap = json.loads(f.read())
+        f.close()
 
-# self-explanatory
-def inBounds(x,y):
-    return 0 <= x and x < len(gameMap) and 0 <= y and y < len(gameMap[0])
+        self.turnNo = 0
+        self.replay = []
 
-#assuming non wrapping map
-def getBotView(ownedTiles):
-    visible = [[False] * len(gameMap[0])] * len(gameMap) #map, init to False
-    for t in ownedTiles:
-        x0 = t['x'] - viewRad
-        y = y0 = t['y'] - viewRad
-        for i in xrange(0,maskSize):
-            x0 += 1
-            if(not inBounds(x0)):
-                continue
-            for j in xrange(0,maskSize):
-                y += 1
-                if(not inBounds(y)):
+    def run(self, botFiles):
+        for i, f in enumerate(botFiles):
+            self.bots[i] = Bot(i, f)
+            self.bots[i].start()
+
+        while self.turnNo < self.maxTurn and len(self.bots) >= 1:
+            self.replay.append([])
+            self.propagateNano()
+            for b in self.bots:
+                b.stdin.write(json.dumps(self.getBotView(b)))
+                # initially only need one line of input
+                self.doTurn(b)
+            self.rmDead()
+
+        for b in self.bots:
+            b.stop()
+
+        # could just write directly; would want to for streaming
+        # buffered writer otherwise
+        f = open('replay', 'w')
+        f.write(json.dumps(self.replay))
+        f.close()
+
+        print "Surviving bots:"
+        for b in self.bots:
+            print b.id
+
+    def doTurn(self, bot):
+        x, y, to, spread = bot.stdout.read().split()
+        ## TODO: validate input
+        owner = bot.id if Terraform.TERRAIN[to] else None
+        base = self.gameMap[x][y] if spread else None
+        tile = Tile(to, owner, base)
+        self.gameMap[x][y] = tile
+
+        if(spread):
+            self.nanoQueue.append({'x': x, 'y': y, 't': tile})
+
+    def propagateNano(self):
+        q = self.nanoQueue
+        self.nanoQueue = None
+        for n in q:
+            for i in xrange(-1,1):
+                for j in xrange(-1,1):
+                    x = n['x'] + i
+                    y = n['y'] + j
+                    if(self.inBounds(x,y) and self.gameMap[x][y].terrain == n['t'].spreadTo):
+                        prev = self.gameMap[x][y]
+                        #update owning bots
+                        if(prev.owner != None):
+                            self.bots[prev.owner].remTer(x, y, prev.terrain)
+                        if(n['t'].owner != None):
+                            self.bots[n['t'].owner].addTer(x, y, n['t'].terrain)
+                        #update map and nanoQueue
+                        self.gameMap[x][y] = n['t']
+                        self.nanoQueue.append({'x': x, 'y': y, 't': n['t']})
+                        self.replay[-1].append(n['t'])
+                        #remove old nano
+                        self.gameMap[n['x']][n['y']].spreadTo = None
+                        self.replay[-1].append({'x': x, 'y': y, 't': self.gameMap[n['x']][n['y']]})
+
+    def inBounds(self, x,y):
+        return 0 <= x and x < len(self.gameMap) and 0 <= y and y < len(self.gameMap[0])
+
+    #assuming non wrapping map
+    def getBotView(self, b):
+        visible = [[False] * len(self.gameMap[0])] * len(self.gameMap)  # map, init to False
+        for (xt,yt) in b.ownedTiles():
+            x0 = xt - self.viewRad
+            y = y0 = yt - self.viewRad
+            for i in xrange(0, len(self.viewMask)):
+                x0 += 1
+                if(not self.inBounds(x0)):
                     continue
-                if viewMask[i][j]:
-                    visible[x0][y] = True
-            y = y0
-    return visible
+                for j in xrange(0, self.maskSize):
+                    y += 1
+                    if(not self.inBounds(y)):
+                        continue
+                    if self.viewMask[i][j]:
+                        visible[x0][y] = True
+                y = y0
+        return visible
+
+    def rmDead(self):
+        for b in self.bots:
+            if len(b.factories) == 0:
+                self.killBot(b)
+
+    def killBot(self, bId):
+        self.bots[bId].stop()
+        del self.bots[bId]
+        print "bot " + bId + " killed."
 
 
-def killBot(id):
-    bots[id].close()
-    del bots[id]
-    print "bot "+i+" killed."
+class Tile(object):
+    """A square of the game map"""
+    def __init__(self, terrain, owner=None, spreadTo=None):
+        super(Tile, self).__init__()
+        self.terrain = terrain
+        self.owner = owner
+        self.spreadTo = spreadTo
 
 
-#main
-parser = argparse.ArgumentParser()
-parser.add_argument('bots', nargs='+',
-                   help='bots to run (currently python)')
-parser.add_argument('-m',
-                   help='map file')
-args = parser.parse_args()
+class Bot(object):
+    """A player bot"""
+    def __init__(self, bId, handle):
+        super(Bot, self).__init__()
+        self.id = bId
+        self.factories = []
+        self.collectors = []
+        self.handle = handle
 
-#read map
-f = open('map', 'r')
-mapData = json.loads(f.read())
-f.close()
+    def start(self):
+        self.io = subprocess.Popen(['python', self.handle], bufsize=1, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        self.stdin = self.io.stdin
+        self.stdout = self.io.stdout
 
-#open bots
-bots = {}
-for i, b in enumerate(args.bots):
-    bots[i] = subprocess.Popen(['python', b], bufsize=1, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    def stop(self):
+        self.io.close()
 
-botTiles = [] * len(bots)
+    # these are almost, but not quite, easy to do functionally in python
+    def addTer(self, x, y, ter):
+        if(ter == 'f'):
+            self.factories.append( (x,y) )
+        elif(ter == 'c'):
+            self.collectors.append( (x,y) )
 
-#run
-maxTurn = 1000
+    def remTer(self, x, y, ter):
+        if(ter == 'f'):
+            self.factories.remove( (x,y) )
+        elif(ter == 'c'):
+            self.collectors.remove( (x,y) )
 
-viewRad = 5
-viewRad2 = viewRad*viewRad
-maskSize = 2*viewRad + 1
-viewMask = [[False] * maskSize] * maskSize
-for i in xrange(0, viewRad):
-    for j in xrange(0, viewRad):
-        if (i*i + j*j <= viewRad2):
-            viewMask[viewRad+i][viewRad+j] = True
-            viewMask[viewRad+i][viewRad-j] = True
-            viewMask[viewRad-i][viewRad+j] = True
-            viewMask[viewRad-i][viewRad-j] = True
+    def ownedTiles(self):
+        return self.factories + self.collectors
 
-turnNo = 0
-gameMap = mapData
-replay = []
 
-TERRAIN = {
-    # generic terrain types
-    1: False,
-    2: False,
-    3: False,
-    # specialized terrain types
-    's': False, # stable (does not propagate) TODO: CHANGE TO STABLE/NON-STABLE CONSTRUCTOR FOR GENERIC TYPES
-    'H': True,
-    'f': True
-} ##
+def __main__():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('bots', nargs='+',
+                       help='bots to run (currently python)')
+    parser.add_argument('-m',
+                       help='map file')
+    args = parser.parse_args()
 
-while(turnNo < 1000 and len(bots) >= 1):
-    replay.append([])
-    for i,b in bots:
-        b.stdin.write(json.dumps(getBotView(botTiles[i]))) #//@
-        # initially only need one line of input
-        doTurn(i, b.stdout.read())
-
-for i,b in bots:
-    b.close()
-
-# could just write directly; would want to for streaming
-# buffered writer otherwise
-f = open('replay', 'w')
-f.write(json.dumps(replay))
-f.close()
-
-print "Surviving bots:"
-for i in bots:
-    print i
-
+    Terraform(args.m).run(args.bots)
